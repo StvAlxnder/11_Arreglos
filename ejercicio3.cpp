840c3c3138c286584256ea4c4b3e8711b6df14c5
StvAlxnder <smamanie@unjbg.edu.pe>
2025-06-12 00:39:33 -0500

    Actualiza ejercicio3: agrega textos para mayor comprensión.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -3,16 +3,22 @@ using namespace std;
 
 int main(){
     int n, A[30], aux;
-    cin >> n;
+    cout << "Intercambiador de elementos equidistantes\n";
+    cout << "Ingrese la cantidad de elementos: ";   cin >> n;
     for (int i = 0; i < n; i++){
-        cin >> A[i];
+        cout << "Ingrese el numero "<<i+1<<": ";  cin >> A[i];
+    }
+    cout << "Numero original : ";
+    for (int i = 0; i < n; i++){
+        cout << A[i];
     }
     for (int i = 0; i < n/2; i++){
         aux = A[i];
         A[i] = A[n - i - 1];
         A[n - i - 1] = aux;
     }
+    cout << "\nNumero invertido: ";
     for (int i = 0; i < n; i++){
-        cout << A[i] << " ";
+        cout << A[i];
     }
 }
