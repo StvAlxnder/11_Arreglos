52bfc4ec6c8a96915e9f8dc12893abc8cb5844c0
StvAlxnder <smamanie@unjbg.edu.pe>
2025-06-11 23:44:51 -0500

    Actualiza ejercicio2: agrega texto para la comprensión del usuario.

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main(){
     int S, n, prom, A[30];
-    cin >> n;
+    cout << "Detector de numeros mayores al promedio\n";
+    cout << "Ingrese la cantidad de numeros a ingresar: ";  cin >> n;
     S = 0;
     for (int i = 0; i < n; i++){
+        cout << "Ingrese el numero "<<i + 1<<": ";
         cin >> A[i];
         S = S + A[i];
     }
     prom = S / n;
+    cout << "El promedio es "<<prom<<", los numeros mayores son:\n";
     for (int i = 0; i < n; i++){
         if (A[i] > prom){
             cout << A[i] << " ";
